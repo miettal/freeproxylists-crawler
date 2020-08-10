f3ccbbfdf1d725a38c3635e0eb9fb31484f83755
Hiromasa Ihara <iharahiromasa@gmail.com>
2020-08-10 10:39:15 +0900

    fix

---
+++ freeproxylists_crawler/utils.py
@@ -0,0 +1,19 @@
+import subprocess
+import json
+
+
+def get_proxies():
+    p = subprocess.Popen(
+        ['python', '-m' 'freeproxylists_crawler/show_proxies'],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    (stdout, stderr) = p.communicate()
+
+    items = []
+    for item_str in stdout.decode('utf-8').strip().split('\n'):
+        item = json.loads(item_str)
+        items.append(item)
+
+    return items
