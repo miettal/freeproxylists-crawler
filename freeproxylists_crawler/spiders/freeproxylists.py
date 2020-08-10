7d1e0c9e98dacab47bc085349b097db745a00c36
Hiromasa Ihara <iharahiromasa@gmail.com>
2020-08-10 10:08:17 +0900

    fix

--- freeproxylists_crawler/spiders/freeproxylists.py
+++ freeproxylists_crawler/spiders/freeproxylists.py
@@ -7,15 +7,19 @@ import scrapy
 
 class FreeproxylistsSpider(scrapy.Spider):
     name = 'freeproxylists'
-    allowed_domains = ['www.freeproxylists.net']
-    start_urls = ['http://www.freeproxylists.net/']
+    # allowed_domains = ['www.freeproxylists.net', 'proxy.moo.jp']
+    # start_urls = ['http://www.freeproxylists.net/', 'http://proxy.moo.jp/']
+    allowed_domains = ['proxy.moo.jp']
+    start_urls = ['http://proxy.moo.jp/']
 
     def parse(self, response):
         tbl = response.css('table')[1]
         for tr in tbl.css('tr'):
             try:
                 ip = urllib.parse.unquote(tr.css('td script::text').re('IPDecode\\("(.+)"\\)')[0])
-                ip = re.match('<a href=".+">(.+)</a>', ip).group(1)
+                m = re.match('<a href=".+">(.+)</a>', ip)
+                if m:
+                    ip = m.group(1)
                 port = int(tr.css('td::text').extract()[0])
                 protocol = 'http' if tr.css('td::text').extract()[1] == 'HTTP' else 'https'
                 rate = float(tr.css('td::text').extract()[6][:-1]) / 100.0
